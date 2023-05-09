c378ca449cc89f2fbc6f476c05dc51b28d89ca0a
Ijas Mohamad <ijas@wednesday.is>
2023-05-09 17:30:49 +0530

    test(fe-cd-react)

--- sst.config.js
+++ sst.config.js
@@ -1,17 +1,17 @@
 import dotenv from "dotenv";
-import { WedNextJsWeb } from "./stacks/WedNextJsWeb";
+import { FrontendReactJsWeb } from "./stacks/FrontendReactJsWeb";
 
 dotenv.config({ path: ".env" });
 
 export default {
-  config(_input) {
-    return {
-      name: process.env.APP_NAME || "web-app",
-      region: process.env.AWS_REGION || "ap-south-1",
-    };
-  },
-  stacks(app) {
-    // deploy stacks
-    app.stack(WedNextJsWeb);
-  },
+	config(_input) {
+		return {
+			name: process.env.APP_NAME || "web-app",
+			region: process.env.AWS_REGION || "ap-south-1",
+		};
+	},
+	stacks(app) {
+		// deploy stacks
+		app.stack(FrontendReactJsWeb);
+	},
 };
--- wed-node-hapi-pg/.babelrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-flow'],
-    plugins: [
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/transform-runtime',
-        '@babel/plugin-syntax-import-assertions'
-    ]
-};
