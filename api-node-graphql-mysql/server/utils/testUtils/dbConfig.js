db926803b708ab2cfb8c02fabd554f809abe7077
Ijas Mohamad <ijas@wednesday.is>
2023-05-11 16:43:48 +0530

    test(negt-pg)

--- api-node-graphql-mysql/server/utils/testUtils/dbConfig.js
+++
@@ -1,7 +0,0 @@
-export const DB_ENV = {
-  DB_URI: 'mysql://user:password@host:3306/table',
-  MYSQL_HOST: 'host',
-  MYSQL_USER: 'user',
-  MYSQL_PASSWORD: 'password',
-  MYSQL_DATABASE: 'table'
-};
