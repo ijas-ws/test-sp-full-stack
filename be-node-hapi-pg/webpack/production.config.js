1ad77d9d320bda1fefbe877aafeac03c5f60af06
Ijas Mohamad <ijas@wednesday.is>
2023-05-11 19:01:42 +0530

    test(react-negt);

--- be-node-hapi-pg/webpack/production.config.js
+++
@@ -1,9 +0,0 @@
-// Important modules this config uses
-const path = require('path');
-
-module.exports = require('./server.config')({
-  mode: 'production',
-  entry: [path.join(process.cwd(), './server.js')],
-
-  plugins: [],
-});
